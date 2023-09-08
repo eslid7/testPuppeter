ce8d5be39feff81c640dcf9756001a51188f3b8c
eslid7 <eslid1@gmail.com>
2023-09-08 08:54:54 -0600

    first commit 2

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer')
+
+
+async function tes(){
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        headless: true,
+    })
+    const page = await browser.newPage()
+    console.log('finish')
+}
+
+// describe(`Testing puperr,`, async() => {
+//     const browser = await puppeteer.launch({
+//         args: ['--no-sandbox'],
+//         headless: true,
+//     })
+//     const page = await browser.newPage()
+//     console.log('finish')
+// })
+// module.exports = { tes }
+
+
+tes();
